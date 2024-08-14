11d0ef1e1719c95315a9153c81b1e954b66905bc
EgorLomonov <32848001+EgorLomonov@users.noreply.github.com>
2024-08-14 22:42:01 +0300

    dcs

--- telegramform/php/send-message-to-telegram.php
+++ telegramform/php/send-message-to-telegram.php
@@ -1,27 +1,53 @@
+
 <?php
-// Получаем данные из формы
-$name = $_POST['name'];
-$phone = $_POST['phone'];
+// Токен телеграм бота
+$tg_bot_token = "7332479910:AAHrlZoFLj0C6Japxjcmd9FX1Ly7zH9MgOs";
+// ID Чата
+$chat_id = "-4207696489";
 
-// Ваш API-ключ Telegram бота
-$telegram_api_key = '7332479910:AAHrlZoFLj0C6Japxjcmd9FX1Ly7zH9MgOs';
+$text = '';
 
-// Чат-идентификатор группы в Telegram, куда будут отправляться уведомления
-$chat_id = '-4207696489';
-
-// Сообщение для отправки в группу
-$message = "Новая заявка на обратный звонок:\nИмя: $name\nНомер телефона: $phone";
-
-// URL для отправки сообщения в Telegram
-$telegram_url = "https://api.telegram.org/bot$telegram_api_key/sendMessage?chat_id=$chat_id&text=" . urlencode($message);
-
-// Отправляем запрос к API Telegram
-$response = file_get_contents($telegram_url);
-
-// Проверяем результат отправки
-if ($response === false) {
-    echo 'Ошибка при отправке заявки.';
-} else {
-    echo 'Заявка успешно отправлена.';
+foreach ($_POST as $key => $val) {
+    $text .= $key . ": " . $val . "\n";
 }
+
+$text .= "\n" . $_SERVER['REMOTE_ADDR'];
+$text .= "\n" . date('d.m.y H:i:s');
+
+$param = [
+    "chat_id" => $chat_id,
+    "text" => $text
+];
+
+$url = "https://api.telegram.org/bot" . $tg_bot_token . "/sendMessage?" . http_build_query($param);
+
+var_dump($text);
+
+file_get_contents($url);
+
+foreach ( $_FILES as $file ) {
+
+    $url = "https://api.telegram.org/bot" . $tg_bot_token . "/sendDocument";
+
+    move_uploaded_file($file['tmp_name'], $file['name']);
+
+    $document = new \CURLFile($file['name']);
+
+    $ch = curl_init();
+
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, ["chat_id" => $chat_id, "document" => $document]);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, ["Content-Type:multipart/form-data"]);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+
+    $out = curl_exec($ch);
+
+    curl_close($ch);
+
+    unlink($file['name']);
+}
+
+die('1');
 ?>
