3407fbf515687f71a342259d5c2abb4892fa69b8
EgorLomonov <32848001+EgorLomonov@users.noreply.github.com>
2024-08-14 23:32:43 +0300

    nbv

--- ajax.php
+++ ajax.php
@@ -1,52 +1,22 @@
 <?php
+$name = $_POST['name']
+$phone = $_POST['phone']
 // Токен телеграм бота
-$tg_bot_token = "7332479910:AAHrlZoFLj0C6Japxjcmd9FX1Ly7zH9MgOs";
+$token = "7332479910:AAHrlZoFLj0C6Japxjcmd9FX1Ly7zH9MgOs";
 // ID Чата
 $chat_id = "-4207696489";
+$arr = array(
+    "Имя" => $name,
+    "Телефон" => $phone
+);
+foreach($arr as $key => $value){
+    $txt .= "<b>".$key."</b>".$value."%0A";
+};
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}", "r");
 
-$text = '';
-
-foreach ($_POST as $key => $val) {
-    $text .= $key . ": " . $val . "\n";
+if ($sendToTelegram){
+    echo'<h1>Спасибо за заявку</h1>';
+    return true
+}else{
+    header('Location:index.html');
 }
-
-$text .= "\n" . $_SERVER['REMOTE_ADDR'];
-$text .= "\n" . date('d.m.y H:i:s');
-
-$param = [
-    "chat_id" => $chat_id,
-    "text" => $text
-];
-
-$url = "https://api.telegram.org/bot" . $tg_bot_token . "/sendMessage?" . http_build_query($param);
-
-var_dump($text);
-
-file_get_contents($url);
-
-foreach ( $_FILES as $file ) {
-
-    $url = "https://api.telegram.org/bot" . $tg_bot_token . "/sendDocument";
-
-    move_uploaded_file($file['tmp_name'], $file['name']);
-
-    $document = new \CURLFile($file['name']);
-
-    $ch = curl_init();
-
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, ["chat_id" => $chat_id, "document" => $document]);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, ["Content-Type:multipart/form-data"]);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-
-    $out = curl_exec($ch);
-
-    curl_close($ch);
-
-    unlink($file['name']);
-}
-
-die('1');
-?>
