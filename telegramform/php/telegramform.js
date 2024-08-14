bf6faba96214969db1712fc48564190e8a4fc242
EgorLomonov <32848001+EgorLomonov@users.noreply.github.com>
2024-08-14 22:54:15 +0300

    Update telegramform.js

--- telegramform/php/telegramform.js
+++ telegramform/php/telegramform.js
@@ -26,7 +26,7 @@ $('.telegram-form').on('submit', function (event) {
     });
     
     $.ajax({
-        url: 'send-message-to-telegram.php',
+        url: './telegramfrom/php/send-message-to-telegram.php',
         type: 'POST',
         data: data,
         cache: false,
